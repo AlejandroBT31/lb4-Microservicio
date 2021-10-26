df27aa1353d10f0c83a49f872b4017d6eaa0e583
AlejandroBT31 <albato31@gmail.com>
2021-10-25 22:50:13 -0500

    port

--- promociones/app.js
+++ promociones/app.js
@@ -4,9 +4,6 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
 
   app.listen(port, () => {
     console.log('servidor en el puerto', port);
