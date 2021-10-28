8897743eeedec3d4fd3505f68db61cd4e952283c
Alejandro Baltazar <57368743+AlejandroBT31@users.noreply.github.com>
2021-10-27 23:49:57 -0500

    Delete index.ts

--- index.ts
+++
@@ -1,5 +0,0 @@
-export * from './service.datasource';
-export * from './servicep.datasource';
-export * from './servicem.datasource';
-export * from './servicec.datasource';
-export * from './monngo-db.datasource';
